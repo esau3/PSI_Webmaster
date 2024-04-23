845e7fddbb028659b35e4a1963ebebaceb3c1edc
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-23 17:37:12 +0100

    Tried to add domain validation

--- webmaster/src/app/website/website.component.ts
+++ webmaster/src/app/website/website.component.ts
@@ -18,9 +18,9 @@ export class WebsiteComponent {
 
     const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
     this.form = this.fb.group({
-      nome: ['', Validators.required, Validators.minLength(3)],
+      nome: ['', [Validators.required, Validators.minLength(3)]],
       url: ['', [Validators.required, Validators.pattern(reg)]], //validaçao com ReGex
-      page: ['', [Validators.required, Validators.pattern(reg),]],
+      page: ['', [Validators.required, Validators.pattern(reg)]],
       importancia: ['', Validators.required] //exemplo
     });
   }
@@ -29,7 +29,7 @@ export class WebsiteComponent {
   website: Website = { // Declare a propriedade 'website' e inicialize-a com um objeto vazio ou com dados iniciais
     id: 0,
     name: "moodle_FCUL",
-    URL: 'https://moodle.ciencias.ulisboa.pt/mod/page/view.php?id=247639',
+    URL: 'https://moodle.ciencias.ulisboa.pt',
     pages: []
   };
 
@@ -38,6 +38,41 @@ export class WebsiteComponent {
   onSelect(website: Website): void {
     this.selectedWebsite = website;
   }
+
+  //o url tem de ser valido senao da barraca
+  removeUrlPrefix(url: string | null): string {
+    // Remove 'https://' ou 'http://'
+    if (url == null) {
+      url = '';
+    }
+    url = url.replace(/^https?:\/\//, '');
+    // Remove o www.
+    url = url.replace(/^www./, '');
+    // Corta tudo após a barra (/)
+    const idx = url.indexOf('/');
+    if (idx !== -1) {
+        url = url.substring(0, idx);
+    }
+    return url;
+  }
+
+  checkDomains() {
+    const urlValue: string = this.form.get('url')?.value;
+    const pageValue: string = this.form.get('page')?.value;
+
+    // Verifica se ambos os valores não são null
+    if (urlValue !== null && pageValue !== null) {
+        const urlWithoutPrefix = this.removeUrlPrefix(urlValue);
+        const pageWithoutPrefix = this.removeUrlPrefix(pageValue);
+        return urlWithoutPrefix === pageWithoutPrefix;
+    }
+
+    // Retorna true se ambos os valores forem null
+    return urlValue === pageValue;
+  }
+
+
+
 
 
   submitForm() {
