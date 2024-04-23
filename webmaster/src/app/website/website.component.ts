0f451caafd633f77f0502a5164ca8b41843b7eb9
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-23 11:48:09 +0100

    Created form to submit a website

--- webmaster/src/app/website/website.component.ts
+++ webmaster/src/app/website/website.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { Website } from '../website';
+import { Website } from '../types';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-website',
@@ -8,6 +9,19 @@ import { Website } from '../website';
   styleUrls: ['./website.component.scss']
 })
 export class WebsiteComponent {
+
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      url: ['', [Validators.required, Validators.pattern(reg)]], //validaçao com ReGex
+      importancia: ['', Validators.required] //exemplo
+    });
+  }
 
   website: Website = { // Declare a propriedade 'website' e inicialize-a com um objeto vazio ou com dados iniciais
     id: 0,
@@ -20,5 +34,13 @@ export class WebsiteComponent {
 
   onSelect(website: Website): void {
     this.selectedWebsite = website;
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      //fazer o tratamento?
+    }
   }
 }
