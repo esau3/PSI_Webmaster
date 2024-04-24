f4c82f7fcbdf0715172dd81588693b34272a5571
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-24 12:29:14 +0100

    changed text to english

--- webmaster/src/app/mock-websites.ts
+++ webmaster/src/app/mock-websites.ts
@@ -188,7 +188,7 @@ function randomDate(start: Date, end: Date): Date {
 }
 
 function randomState(): string {
-    const states = ['Em avaliação', 'Avaliado', 'Erro na avaliação'];
+    const states = ['under evaluation', 'evaluated', 'error in evaluation'];
     const index = Math.floor(Math.random() * states.length);
     return states[index];
 }
