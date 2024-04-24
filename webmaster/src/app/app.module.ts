95dce82e2a04ae8f0c134641a251fcf850023e5a
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-24 12:16:07 +0100

    Sort and Paginator implemented in the Websites Table

--- webmaster/src/app/app.module.ts
+++ webmaster/src/app/app.module.ts
@@ -22,6 +22,8 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,9 @@ import {MatTableModule} from '@angular/material/table';
     MatListModule,
     MatIconModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webmaster/src/app/websites-table/websites-table.component.ts
+++ webmaster/src/app/websites-table/websites-table.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { WEBSITES } from '../mock-websites';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Website } from '../types';
 
 
 @Component({
@@ -12,11 +15,28 @@ import { WEBSITES } from '../mock-websites';
 export class WebsitesTableComponent {
   
   displayedColumns: string[] = ['id', 'name', 'URL', 'pages', 'register_date', 'eval_date', 'monitor_state'];
-  dataSource = new MatTableDataSource(WEBSITES);
+  dataSource: MatTableDataSource<Website>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(WEBSITES);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
