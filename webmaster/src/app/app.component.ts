11b2bb70203ea4cf6561aa438b85cb298b2c96ba
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-27 15:45:00 +0100

    Toggle drawer fixed

--- webmaster/src/app/app.component.ts
+++ webmaster/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Webmaster';
+
+  drawer = true;
+
+  toggleDrawer() {
+    this.drawer = !this.drawer; // Alterna entre aberto e fechado
+  }
+
+  getDrawerValue() {
+    return this.drawer;
+  }
+
 }
