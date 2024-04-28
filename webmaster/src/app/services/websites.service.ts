1ecb201bed67d8c562d2a33726b9e92e3246ffda
fc57544 <rafaelmmf19062000@gmail.com>
2024-04-28 13:09:59 +0100

    debug5

--- webmaster/src/app/services/websites.service.ts
+++ webmaster/src/app/services/websites.service.ts
@@ -8,14 +8,14 @@ import { Page, Website } from '../types';
 @Injectable({ providedIn: 'root' })
 export class WebsiteService {
 
-  private websiteUrl = "http://10.101.151.25:3092/website";
-  private websitesUrl = "http://10.101.151.25:3092/websites";
+  private websitesUrl = "http://10.101.151.25:3092/website";
+  private websiteUrl = "http://10.101.151.25:3092/websites";
   constructor(
     private http: HttpClient) { }
 
     /** GET websites from the server */
   getWebsites(): Observable<Website[]> {
-    return this.http.get<Website[]>(this.websitesUrl).pipe(
+    return this.http.get<Website[]>(this.websiteUrl).pipe(
       catchError(this.handleError)
     );
   }
@@ -25,16 +25,16 @@ export class WebsiteService {
   getWebsite(_id: number): Observable<Website> {
     // For now, assume that a website with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-    const url=this.websitesUrl+"/"+_id.toString();
-     return this.http.get<Website>(this.websitesUrl)
+    const url=this.websiteUrl+"/"+_id.toString();
+     return this.http.get<Website>(this.websiteUrl)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   deleteWebsite(_id:number):Observable<Website>{
-    const url=this.websitesUrl+"/"+_id.toString();
-    return this.http.delete<Website>(this.websitesUrl)
+    const url=this.websiteUrl+"/"+_id.toString();
+    return this.http.delete<Website>(this.websiteUrl)
    .pipe(
      catchError(this.handleError)
    );
@@ -42,7 +42,7 @@ export class WebsiteService {
 
   postWebsite(websiteData: string): Observable<any> {
 
-    return this.http.post(this.websiteUrl, websiteData)
+    return this.http.post(this.websitesUrl, websiteData)
       .pipe(
         catchError(this.handleError)
       );
@@ -50,7 +50,7 @@ export class WebsiteService {
 
   getPage(_id:number):Observable<Page>{
     const url="page/"+_id.toString();
-    return this.http.delete<Page>(this.websitesUrl)
+    return this.http.delete<Page>(this.websiteUrl)
    .pipe(
      catchError(this.handleError)
    );
