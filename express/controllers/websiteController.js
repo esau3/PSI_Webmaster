84763196c0ddadce131e0b2ad99103d495ecf02e
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-06 16:59:23 +0100

    debug13

--- express/controllers/websiteController.js
+++ express/controllers/websiteController.js
@@ -85,12 +85,16 @@ exports.website_delete = asyncHandler(async (req, res, next) => {
 exports.page_update = asyncHandler(async (req, res, next) => {
   
     const website = await Website.findById(req.params.id).exec();
-
+    res.send(JSON.stringify(website));
+    if (website === null) {
+      const err = new Error("Website not found");
+      err.status = 404;
+      return next(err);
+    }
 
     website.pages.push(req.body).exec(); 
 
     await website.save().exec();
-    res.send("Page added to website successfully");
 });
 
 
