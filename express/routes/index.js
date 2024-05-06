a268bd3f85964a0c2cf69c35f8e487bcd502ceea
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-06 16:01:22 +0100

    typo

--- express/routes/index.js
+++ express/routes/index.js
@@ -33,6 +33,6 @@ router.get("/page/:id", page_controller.page_detail);
 router.get("/website/:id/eval", website_controller.website_eval);
 
 // PUT request to update a Page.
-router.put("/websites/:id/pages/:pageId", websiteController.page_update);
+router.put("/websites/:id/pages/:pageId", website_controller.page_update);
 
 module.exports = router;
