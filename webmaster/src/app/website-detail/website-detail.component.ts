bc6a58667dc7015435d7d6d4fcbbffab575c8866
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-07 17:18:40 +0100

    debug13

--- webmaster/src/app/website-detail/website-detail.component.ts
+++ webmaster/src/app/website-detail/website-detail.component.ts
@@ -60,7 +60,7 @@ export class WebsiteDetailComponent implements OnInit {
     }
 
   getPages(): void {
-    if (this.website && this.website.pages.length === 0) {
+    if (this.website && this.website?.pages.length === 0) {
       console.log(this.website.pages);
       const pages = this.website.pages;
       
@@ -72,7 +72,7 @@ export class WebsiteDetailComponent implements OnInit {
           });
       }
     }
-    console.log('AQUI!');
+    console.log('AQUI!: ', this.website?.pages.length);
   }
 
   updatePages() {
