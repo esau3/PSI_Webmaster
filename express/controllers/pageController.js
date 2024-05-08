6c359786f9dbad2031b85b668617f035f979b1f0
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-08 10:23:45 +0100

    report to json

--- express/controllers/pageController.js
+++ express/controllers/pageController.js
@@ -93,7 +93,7 @@ exports.page_eval = asyncHandler(async (req, res, next) => {
   // executar a avaliação, recebendo o relatório
   const report = await qualweb.evaluate(qualwebOptions);
   const earlReport = generateEARLReport(report, earlOptions);
-  res(earlReport);
+  res.json(earlReport);
   // parar o avaliador e libertar recursos
   await qualweb.stop();
 });
