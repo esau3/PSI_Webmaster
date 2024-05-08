98ab93dd6c7fac38ee3159e305f7f43eb1660735
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-08 12:33:43 +0100

    created rule metadata structure

---
+++ express/models/rules-metadata.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const RuleMetadataSchema = new Schema({
+    code: { type: String, required: true, maxLength: 50 },
+    name: { type: String, required: true, maxLength: 500 },
+    passed: { type: Number, required: true},
+    warning: { type: Number, required: true},
+    failed: { type: Number, required: true},
+    inapplicable: { type: Number, required: true},
+    type: {
+        type: [String],
+        validate: {
+          validator: function (v) {
+            return v.every(val => ["A", "AA", "AAA"].includes(val));
+          }
+        }
+    }
+});
+
+
+
+// Virtual for website's URL
+WebsiteSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/rule-metadata/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("RuleMetadata", RuleMetadataSchema);
