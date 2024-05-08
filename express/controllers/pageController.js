e85bd2c2003b13ebc4a95d9a2856eb507812314b
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-08 15:46:50 +0100

    debug13

--- express/controllers/pageController.js
+++ express/controllers/pageController.js
@@ -83,6 +83,7 @@ exports.page_eval = asyncHandler(async (req, res, next) => {
 
   //ir buscar o url correto
   let toEval = page.page_URL;
+  console.log(toEval);
 
   // especificar as opções, incluindo o url a avaliar
   const qualwebOptions = {
