275441c46a54d662547bc206763f07ed8db0b405
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-08 15:37:57 +0100

    debug10

--- express/controllers/pageController.js
+++ express/controllers/pageController.js
@@ -1,4 +1,4 @@
-const file = require("./663b48bf5ba7461b74c8abc6.json");
+const file = require("../663b48bf5ba7461b74c8abc6.json");
 const Page = require("../models/page");
 const Website = require("../models/website");
 const ReportMetadata = require("../models/report-metadata");
