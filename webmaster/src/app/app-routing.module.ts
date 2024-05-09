4716879c72d433a8ae4bd5709b685528724f504f
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-09 11:45:11 +0100

    added path to routing

--- webmaster/src/app/app-routing.module.ts
+++ webmaster/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { WebsiteComponent } from './website/website.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { WebsitesTableComponent } from './websites-table/websites-table.component';
 import { WebsiteDetailComponent } from './website-detail/website-detail.component';
+import { ReportDetailComponent } from './report-detail/report-detail.component';
 
 const routes: Routes = [
   { path: 'website', component: WebsiteComponent },
   { path: 'side-nav', component: SideNavComponent },
   { path: 'websites-table', component: WebsitesTableComponent },
   { path: 'website-detail/:id', component: WebsiteDetailComponent },
+  { path: 'report-detail/:id', component: ReportDetailComponent },
 ];
 
 @NgModule({
