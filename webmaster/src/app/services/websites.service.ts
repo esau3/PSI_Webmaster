265d9e8479694cf228482fcb568cfc19ad7987bd
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-10 11:48:36 +0100

    cleaned code

--- webmaster/src/app/services/websites.service.ts
+++ webmaster/src/app/services/websites.service.ts
@@ -8,10 +8,11 @@ import { Page, Report, Rule, Website } from '../types';
 @Injectable({ providedIn: 'root' })
 export class WebsiteService {
 
-  private website = "http://10.101.151.25:3092/website";
+  private website = "http://10.101.151.25:3092/website/";
   private websites = "http://10.101.151.25:3092/websites";
-  private page = "http://10.101.151.25:3092/page";
-  private eval_page = "http://10.101.151.25:3092/eval";
+  private page = "http://10.101.151.25:3092/page/";
+  private eval_page = "http://10.101.151.25:3092/eval/";
+  private eval_pages = "http://10.101.151.25:3092/evals";
   constructor(
     private http: HttpClient) { }
 
@@ -22,33 +23,33 @@ export class WebsiteService {
     );
   }
 
-    /** GET websites from the server */
-    getPages(): Observable<Page[]> {
-      return this.http.get<Page[]>(this.page).pipe(
-        catchError(this.handleError)
-      );
-    }
+  /** GET websites from the server */
+  getPages(): Observable<Page[]> {
+    return this.http.get<Page[]>(this.page).pipe(
+      catchError(this.handleError)
+    );
+  }
 
     // URL to web api
 
   getWebsite(_id: string): Observable<Website> {
     // For now, assume that a website with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-     return this.http.get<Website>(this.website + '/' + _id)
+     return this.http.get<Website>(this.website + _id)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   deleteWebsite(_id:string):Observable<Website>{
-    return this.http.delete<Website>(this.website + '/' + _id)
+    return this.http.delete<Website>(this.website + _id)
    .pipe(
      catchError(this.handleError)
    );
   }
 
   deletePage(id:string):Observable<Page>{
-    return this.http.delete<Page>(this.page + '/' + id)
+    return this.http.delete<Page>(this.page + id)
    .pipe(
      catchError(this.handleError)
    );
@@ -61,14 +62,14 @@ export class WebsiteService {
       );
   }
   startEvaluation(_id: string):Observable<any>{
-    return this.http.get<any>(this.eval_page + '/' + _id)
+    return this.http.get<any>(this.eval_page + _id)
    .pipe(
      catchError(this.handleError)
    );
   }
 
   getPage(_id: string):Observable<Page>{
-    return this.http.get<Page>(this.page + '/' + _id)
+    return this.http.get<Page>(this.page + _id)
    .pipe(
      catchError(this.handleError)
    );
@@ -77,36 +78,37 @@ export class WebsiteService {
 
   //pagina a adicionar e id do website para colocar a pagina
   putPage(pageData: any, id: string) {
-    return this.http.put<Page>(this.website + '/' + id, pageData)
+    return this.http.put<Page>(this.website + id, pageData)
     //return this.http.put<Page>(this.page + '/' + id , pageData)
     .pipe(
       catchError(this.handleError)
     );
   }
 
+  //wrong urls
   getReport(id:string):Observable<Report>{
-    return this.http.get<Report>(this.page + '/report/' + id)
+    return this.http.get<Report>(this.eval_page + id)
    .pipe(
      catchError(this.handleError)
    );
   }
-
+//wrong urls
   getReports():Observable<Report[]>{
-    return this.http.get<Report[]>(this.page + '/report')
+    return this.http.get<Report[]>(this.eval_pages)
     .pipe(
       catchError(this.handleError)
     );
   }
 
   getRules():Observable<Rule[]>{
-    return this.http.get<Rule[]>(this.page + '/rule')
+    return this.http.get<Rule[]>(this.eval_pages)
    .pipe(
      catchError(this.handleError)
    );
   }
 
   getRule(id:string):Observable<Rule>{
-    return this.http.get<Rule>(this.page + '/rule/'+id)
+    return this.http.get<Rule>(this.eval_page + id)
     .pipe(
       catchError(this.handleError)
     );
