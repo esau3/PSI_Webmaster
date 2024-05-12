24db15e249acf0ce0ae881a8209187db53035273
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-12 10:08:20 +0100

    debug10

--- webmaster/src/app/website-detail/website-detail.component.ts
+++ webmaster/src/app/website-detail/website-detail.component.ts
@@ -82,8 +82,8 @@ export class WebsiteDetailComponent implements OnInit {
             }
             this.pages.push(pageData);
             //console.log("get pages", this.pages);
-            console.log("id do report?", pageData.report);
-            this.getReport(String(pageData.report)); 
+            //console.log("id do report?", pageData.report);
+            //this.getReport(String(pageData.report)); 
           });
       }
       
