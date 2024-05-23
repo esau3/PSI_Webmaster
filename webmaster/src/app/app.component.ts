19921029d1a0a9fb29095c15363f042d5e425b22
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-23 13:50:15 +0100

    trying to remove keyboard interactions

--- webmaster/src/app/app.component.ts
+++ webmaster/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { arrowComponent } from './arrow.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends arrowComponent {
+//export class AppComponent extends arrowComponent {
+export class AppComponent {
   title = 'Webmaster';
 
   drawer = true;
