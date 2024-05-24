a3c3a89801879a0d33a9899a2a0d9cab51ff781a
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-24 12:20:30 +0100

    keep trying

--- webmaster/src/app/services/pdf-save.service.ts
+++ webmaster/src/app/services/pdf-save.service.ts
@@ -19,20 +19,27 @@ export class PdfSaveService {
 
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i] as HTMLElement;
-      const canvas = await html2canvas(element, { scale: 3 }); // Aumentando a escala para melhorar a qualidade
+      
+      // Obtemos a altura total do elemento, incluindo seu conteúdo e margens
+      const elementHeight = element.getBoundingClientRect().height;
+
+      // Limitamos a altura do elemento para a altura da página
+      element.style.height = '100%';
+
+      const canvas = await html2canvas(element, { scale: 2, useCORS: true }); // Ajustamos a escala e habilitamos o uso de CORS
       const imgHeight = canvas.height * imgWidth / canvas.width;
-      const contentDataURL = canvas.toDataURL('image/png');
+      const contentDataURL = canvas.toDataURL('image/jpeg', 0.7); // Ajustamos a qualidade da imagem
 
       if (i > 0) {
-        pdf.addPage(); // Adiciona uma nova página para todos os elementos, exceto o primeiro
+        pdf.addPage(); // Adicionamos uma nova página para todos os elementos, exceto o primeiro
       }
       
       if (yOffset + imgHeight > pageHeight) {
-        yOffset = 0; // Reseta o deslocamento quando adicionamos uma nova página
+        yOffset = 0; // Resetamos o deslocamento quando adicionamos uma nova página
         pdf.addPage();
       }
 
-      pdf.addImage(contentDataURL, 'PNG', 0, yOffset, imgWidth, imgHeight);
+      pdf.addImage(contentDataURL, 'JPEG', 0, yOffset, imgWidth, imgHeight);
       yOffset += imgHeight;
     }
 
