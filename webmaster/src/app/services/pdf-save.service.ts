bf8c6cfa15ce7787974ced1be4856c8d1f7ddc02
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-24 13:55:20 +0100

    debug2

--- webmaster/src/app/services/pdf-save.service.ts
+++ webmaster/src/app/services/pdf-save.service.ts
@@ -9,26 +9,23 @@ export class PdfSaveService {
 
   constructor() { }
 
-  async generatePDF(fileName: string) {
+  generatePDF(fileName: string) {
 
-    const elements: any = document.getElementsByClassName('pdf_content');
-    
-    html2canvas(elements, {scale: 2}).then((canvas) => {
+      const elements: any = document.getElementsByClassName('pdf_content');
+      
+      html2canvas(elements, {scale: 2}).then((canvas) => {
 
-      const pdf = new jspdf();
+        const pdf = new jspdf();
 
-      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
 
-      pdf.setProperties({
-        title: "ALOOO",
-        subject: "QualWeb Report",
-        author: "Grupo 42"
+        pdf.setProperties({
+          title: "ALOOO",
+          subject: "QualWeb Report",
+          author: "Grupo 42"
+        })
+
+        pdf.save(fileName + ".pdf");
       })
-
-      pdf.save(fileName + ".pdf");
-    }
-  ) 
-
-  
   }
 }
