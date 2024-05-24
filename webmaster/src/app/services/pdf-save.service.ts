ba95a96eb8fad76e94b3adc07eb37e37b399b0bd
fc57544 <rafaelmmf19062000@gmail.com>
2024-05-24 14:08:09 +0100

    Pages were inside one another

--- webmaster/src/app/services/pdf-save.service.ts
+++ webmaster/src/app/services/pdf-save.service.ts
@@ -11,24 +11,40 @@ export class PdfSaveService {
 
   generatePDF(fileName: string) {
 
-      const elements: any = document.getElementsByClassName('pdf_content');
+    const elements: any = document.getElementsByClassName('pdf_content');
+    const promises: Promise<HTMLCanvasElement>[] = [];
 
-      for (let i = 0; i < elements.length; i++) {
-        
-        html2canvas(elements[i], {scale: 2}).then((canvas) => {
-
-          const pdf = new jspdf();
-
-          pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-
-          pdf.setProperties({
-            title: fileName + ".pdf",
-            subject: "QualWeb Report",
-            author: "Grupo 42"
-          })
-
-          pdf.save(fileName + ".pdf");
+    for (let i = 0; i < elements.length; i++) {
+      promises.push(
+        new Promise((resolve, reject) => {
+          html2canvas(elements[i], { scale: 2 }).then((canvas) => {
+            resolve(canvas);
+          }).catch(error => {
+            reject(error);
+          });
         })
+      );
     }
+
+    Promise.all(promises).then((canvases) => {
+      const pdf = new jspdf();
+
+      canvases.forEach((canvas, index) => {
+        if (index !== 0) {
+          pdf.addPage(); // Adiciona uma nova página para os elementos subsequentes
+        }
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+      });
+
+      pdf.setProperties({
+        title: fileName + ".pdf",
+        subject: "QualWeb Report",
+        author: "Grupo 42"
+      });
+
+      pdf.save(fileName + ".pdf");
+    }).catch(error => {
+      console.error('Erro ao gerar o PDF:', error);
+    });
   }
 }
