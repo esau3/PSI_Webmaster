dd3348b9abc6d1806058099440e1edd0ac474cef
aluno-di <duarte.pi2001@gmail.com>
2024-05-24 06:51:40 +0100

    dim debug

--- webmaster/src/app/services/pdf-save.service.ts
+++ webmaster/src/app/services/pdf-save.service.ts
@@ -17,8 +17,8 @@ export class PdfSaveService {
       for (let i = 0; i < elements.length; i++) {
         const element = elements[i] as HTMLElement;
         const canvas = await html2canvas(element);
-        const imgWidth = 208;
-        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const imgWidth = canvas.width;
+        const imgHeight = canvas.height ;//* imgWidth / canvas.width;
         const contentDataURL = canvas.toDataURL('image/png');
 
         pdf.addPage()
